fdb5233a2b19bc24e063212eb3894cf680d15871
Alex Borges <alexborgesramos2002@gmail.com>
2026-05-25 09:11:35 -0300

    Adds GoRouter

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:fin_system/features/home/presentation/home_page.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'app_router.g.dart';
+
+@riverpod
+GoRouter appRouter(Ref ref) {
+  return GoRouter(
+    initialLocation: '/',
+    debugLogDiagnostics: true,
+    routes: [
+      GoRoute(
+        path: '/',
+        name: 'home',
+        builder: (context, state) => const HomePage(),
+      ),
+    ],
+  );
+}
